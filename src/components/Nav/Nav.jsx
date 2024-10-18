8aa19f35e9ad8dec9f454177c861d8fd7440d52b
RenatoFBabi <renatofrancosobabichak@gmail.com>
2024-10-18 11:08:34 -0300

    Fazendo uma API

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
-    <div>
+    <header className="bg-blue-950 text-white p-6">
+        <h1 className="text-3xl font-bold text-center mb-2">Projeto Exemplo 8</h1>
       <Link to='/'>Home</Link>
-    </div>
+    </header>
   )
 }
 
